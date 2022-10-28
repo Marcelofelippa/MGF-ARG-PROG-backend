c650f571e3d1afd06290943c3969028fce3a34f9
Marcelofelippa <98492760+Marcelofelippa@users.noreply.github.com>
2022-10-28 11:30:38 -0300

    Add files via upload

---
+++ mgf/src/test/java/com/portfolio/mgf/MgfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mgf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
